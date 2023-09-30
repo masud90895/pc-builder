9089452d534f778d93251b64db9ac6df439777dd
masud90895 <masudhossainmbs129@gmail.com>
2023-09-30 15:28:37 +0600

    mordal banner replaced

--- src/components/ui/banner.js
+++ src/components/ui/banner.js
@@ -1,27 +1,45 @@
+import Image from "next/image";
 import React from "react";
 
 const Banner = () => {
   return (
-    <div className="p-6 py-12 dark:bg-violet-400 dark:text-gray-900 lg:w-[1170px] lg:mx-auto">
-      <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row items-center justify-between">
-          <h2 className="text-center text-6xl tracki font-bold">
-            Up to 50% Off <br />
-          </h2>
-          <div className="space-x-2 text-center py-2 lg:py-0">
-            <span>Plus free shipping! Use code:</span>
-            <span className="font-bold text-lg">ELONMUSK</span>
+
+    <div class="container mx-auto  px-4 md:px-6">
+      <div class="flex items-strech justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8">
+        <div class="flex flex-col md:flex-row items-strech justify-between bg-gray-50 dark:bg-gray-800 py-6 px-6 md:py-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-9/12">
+          <div class="flex flex-col justify-center md:w-1/2">
+            <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">Best Deal</h1>
+            <p class="text-base lg:text-xl text-gray-800 dark:text-white mt-2">Save upto <span class="font-bold">50%</span></p>
           </div>
-          <a
-            href="#"
-            rel="noreferrer noopener"
-            className="px-5 mt-4 lg:mt-0 py-3 rounded-md border block dark:bg-gray-50 dark:text-gray-900 dark:border-gray-400"
-          >
-            Shop Now
-          </a>
+          <div class="md:w-1/2 mt-8 md:mt-0 flex justify-center md:justify-end">
+
+            <Image
+              alt=""
+              src={"https://i.ibb.co/J2BtZdg/Rectangle-56-1.png"}
+              width={304}
+              height={171}
+            />
+          </div>
+        </div>
+        <div class="md:w-4/12 lg:w-5/12 xl:w-4/12 2xl:w-3/12 bg-gray-50 dark:bg-gray-800 py-6 px-6 md:py-0 md:px-4 lg:px-6 flex flex-col justify-center relative">
+          <div class="flex flex-col justify-center">
+            <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">Game Console</h1>
+            <p class="text-base lg:text-xl text-gray-800 dark:text-white">Save Upto <span class="font-bold">30%</span></p>
+          </div>
+          <div class="flex justify-end md:absolute md:bottom-4 md:right-4 lg:bottom-0 lg:right-0">
+
+            <Image
+              alt=""
+              src={"https://i.ibb.co/rGfP7mp/Rectangle-59-1.png"}
+              width={145}
+              height={143}
+              className="md:w-20 md:h-20 lg:w-full lg:h-full"
+            />
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
